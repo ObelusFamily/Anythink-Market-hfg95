54deafd49903d50cd52b0a5de1c156e7d6094e58
KaushikKC <kccreations1704@gmail.com>
2022-08-04 21:31:58 +0530

    Fixed Dataseeding

--- backend/scripts/seeds.js
+++ backend/scripts/seeds.js
@@ -1,7 +1,7 @@
 //TODO: seeds script should come here, so we'll be able to put some data in our local env
 import {faker} from '@faker-js/faker';
 
-
+var mongoose = require('mongoose');
 mongoose.connect('process.env.MONGODB_URI');
 
 var Items = require('../models/Item')
