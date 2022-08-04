c17f0eae66882bf066e4c65098a151f889e58b01
KaushikKC <kccreations1704@gmail.com>
2022-08-04 21:36:15 +0530

    Fixed Dataseeding

--- backend/scripts/seeds.js
+++ backend/scripts/seeds.js
@@ -1,14 +1,15 @@
 //TODO: seeds script should come here, so we'll be able to put some data in our local env
 import {faker} from '@faker-js/faker';
+import Item from '../models/Item'
+import User from '../models/User'
 
 var mongoose = require('mongoose');
 mongoose.connect('process.env.MONGODB_URI');
 
-var Items = require('../models/Item')
-var Users = require('../models/User');
+
 
 for (let i = 0; i < 100; i++) {
-    const user = new Users({
+    const user = new User({
         name: faker.internet.userName(),
         email: faker.internet.email()
     })
@@ -16,10 +17,11 @@ for (let i = 0; i < 100; i++) {
     user.save()
     .then(userRef => {
         console.log(`${userRef.name} saved successfully`);
-        const item = new Items({
+        const item = new Item({
             title: user.name,
             description: faker.random.words(),
             image: faker.image.abstract()
         })
+        item.save()
     })
 }
