8b686c6087fc2db42d378438c450f202bc74a75d
KaushikKC <kccreations1704@gmail.com>
2022-08-04 21:42:10 +0530

    Fixed module-found

--- backend/scripts/seeds.js
+++ backend/scripts/seeds.js
@@ -1,12 +1,14 @@
 //TODO: seeds script should come here, so we'll be able to put some data in our local env
-import {faker} from '@faker-js/faker';
-import Item from '../models/Item'
-import User from '../models/User'
+// import {faker} from '';
+// import Item from '../models/Item'
+// import User from '../models/User'
+var faker = require('@faker-js/faker')
 
 var mongoose = require('mongoose');
 mongoose.connect('process.env.MONGODB_URI');
 
-
+var Item = require('../models/Item')
+var User = require('../models/User')
 
 for (let i = 0; i < 100; i++) {
     const user = new User({
